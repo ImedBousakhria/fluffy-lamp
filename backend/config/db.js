0e69ad25b13f70ff62d098b047f093efca27a857
imedBousakhria <i_bousakhria@estin.dz>
2025-10-06 07:37:38 +0200

    BACK fix: now it connects to the correct db from the cluster0

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected: ${conn.connection.host} / DB: ${conn.connection.name}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
